c5ce2c0719a6c4d6ab0719543e60b8d9f7795636
S-E-A-JS <9168441394@bk.ru>
2022-09-14 17:08:23 +0300

    используя useState реализована возможность увеличивать и уменьшать счетчик

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const formatCount = () => {
+        return count === 0 ? <h1>empty</h1> : count
+    };
+    const getBadgeClasses = () => {
+        classes += count === 0 ? 'bg-warning' : 'bg-primary'
+        return classes;
+    }
+    let classes = 'badge m-2 ';
+
+    const handleIncrement = () => {
+        setCount((prevState) => prevState + 1);
+    }
+    const handleDecrement = () => {
+        setCount((prevState) => prevState - 1);
+    }
     return (
         <>
-            <h1>Counter</h1>
-            <button>+</button>
+            <span className={getBadgeClasses()}>{formatCount()}</span>
+            <button className={'btn btn-primary m-2'} onClick={handleIncrement}>+</button>
+            <button className={'btn btn-primary m-2'} onClick={handleDecrement}>-</button>
         </>
     )
 };
